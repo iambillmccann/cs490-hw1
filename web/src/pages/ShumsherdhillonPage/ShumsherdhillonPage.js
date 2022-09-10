b99f441526e94172aede049219f2add1993876e7
Shumsher <shumsherdhillon@gmail.com>
2022-09-10 16:03:17 -0400

    Added picture and modified .js file

--- web/src/pages/ShumsherdhillonPage/ShumsherdhillonPage.js
+++ web/src/pages/ShumsherdhillonPage/ShumsherdhillonPage.js
@@ -9,7 +9,6 @@ const ShumsherdhillonPage = () => {
       <h1 class="center">ShumsherdhillonPage, wow</h1>
       
       <img src="https://joncomics.files.wordpress.com/2018/08/pranks.png" alt="The essence of comedy" class="center"></img>
-	<div <iframe src="https://streamable.com/e/y6t463" frameborder="0" width="100%" height="100%"></iframe></div>
 
     </>
   )
