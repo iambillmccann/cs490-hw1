07708080faa204e5a769e074ff74019640f4ed49
Shumsher <shumsherdhillon@gmail.com>
2022-09-10 15:54:55 -0400

    Added picture and modified .js file

--- web/src/pages/ShumsherdhillonPage/ShumsherdhillonPage.js
+++ web/src/pages/ShumsherdhillonPage/ShumsherdhillonPage.js
@@ -9,7 +9,7 @@ const ShumsherdhillonPage = () => {
       <h1 class="center">ShumsherdhillonPage, wow</h1>
       
       <img src="https://joncomics.files.wordpress.com/2018/08/pranks.png" alt="The essence of comedy" class="center"></img>
-	<div style="width:100%;height:0px;position:relative;padding-bottom:183.125%;"><iframe src="https://streamable.com/e/y6t463" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
+	<div <iframe src="https://streamable.com/e/y6t463" frameborder="0" width="100%" height="100%" allowfullscreen></iframe></div>
 
     </>
   )
