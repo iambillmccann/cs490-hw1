44ace64737b97531fc69e0436592cde9c2d969c6
LazarAgoev <91933346+LazarAgoev@users.noreply.github.com>
2022-09-10 20:41:01 -0400

    Through github

--- web/src/pages/LazaragoevPage/LazaragoevPage.js
+++ web/src/pages/LazaragoevPage/LazaragoevPage.js
@@ -6,14 +6,8 @@ const LazaragoevPage = () => {
     <>
       <MetaTags title="Lazaragoev" description="Lazaragoev page" />
 
-      <h1>LazaragoevPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/LazaragoevPage/LazaragoevPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>lazaragoev</code>, link to me with `
-        <Link to={routes.lazaragoev()}>Lazaragoev</Link>`
-      </p>
+      <h1>LazarAgoevPage</h1>
+      <img src= "https://i.redd.it/vlxhtljarvm91.jpg" alt="Chimaev vs Diaz" width="500" height="600" />
     </>
   )
 }
