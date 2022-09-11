8a7668b5df2696c0f8d35f90508dd81057350098
Aleyna Aydin <aga32@njit.edu>
2022-09-11 02:08:41 -0400

    Revert "Merge branch 'hw1-aleyna' of https://github.com/iambillmccann/cs490-hw1 into hw1-aleyna"
    This reverts commit 5c19f3e063714c468fb7b964480f9b2d75773fb2, reversing
    changes made to 528cd014c8aac76060b34d52b4500232bff52291.

--- web/src/pages/SakthivelanmanikanthanPage/SakthivelanmanikanthanPage.js
+++ web/src/pages/SakthivelanmanikanthanPage/SakthivelanmanikanthanPage.js
@@ -5,13 +5,23 @@ const SakthivelanmanikanthanPage = () => {
   return (
     <>
       <MetaTags
-        title="Sakthivelanmanikanthan" description="Sakthivelanmanikanthan page" />
-
-      <img
-	className="center"
-	src="https://media.giphy.com/media/jUwpNzg9IcyrK/giphy.gif"
-	alt="Nope"
+        title="Sakthivelanmanikanthan"
+        description="Sakthivelanmanikanthan page"
       />
+
+      <h1>SakthivelanmanikanthanPage</h1>
+      <p>
+        Find me in{' '}
+        <code>
+          ./web/src/pages/SakthivelanmanikanthanPage/SakthivelanmanikanthanPage.js
+        </code>
+      </p>
+      <p>
+        My default route is named <code>sakthivelanmanikanthan</code>, link to
+        me with `
+        <Link to={routes.sakthivelanmanikanthan()}>Sakthivelanmanikanthan</Link>
+        `
+      </p>
     </>
   )
 }
