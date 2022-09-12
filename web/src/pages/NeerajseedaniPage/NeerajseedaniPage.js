117c222588ab0153ab7589b0a93cf398183b5f50
Neeraj Seedani <ns922@njit.edu>
2022-09-11 22:46:43 -0400

    Neeraj Seedani's Commit

--- web/src/pages/NeerajseedaniPage/NeerajseedaniPage.js
+++ web/src/pages/NeerajseedaniPage/NeerajseedaniPage.js
@@ -4,17 +4,14 @@ import { MetaTags } from '@redwoodjs/web'
 const NeerajseedaniPage = () => {
   return (
     <>
-      <MetaTags title="Neerajseedani" description="Neerajseedani page" />
+      <MetaTags title="Neerajseedani" description="Neeraj Seedani's page" />
 
-      <h1>NeerajseedaniPage</h1>
-      <p>
-        Find me in{' '}
-        <code>./web/src/pages/NeerajseedaniPage/NeerajseedaniPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>neerajseedani</code>, link to me with `
-        <Link to={routes.neerajseedani()}>Neerajseedani</Link>`
-      </p>
+      <img
+        className="center"
+        src="https://giphy.com/explore/monkey-puppet"
+        alt="Monkey Puppet Meme"
+      />
+
     </>
   )
 }
