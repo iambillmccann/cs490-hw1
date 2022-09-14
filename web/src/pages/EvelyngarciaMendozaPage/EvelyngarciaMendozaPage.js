6d45905eb7bf6b133158545238c76260264797c6
Evelyn-Garcia <71730349+Evelyn-Garcia@users.noreply.github.com>
2022-09-14 13:15:42 -0400

    Update EvelyngarciaMendozaPage.js
    edited conflicts

--- web/src/pages/EvelyngarciaMendozaPage/EvelyngarciaMendozaPage.js
+++ web/src/pages/EvelyngarciaMendozaPage/EvelyngarciaMendozaPage.js
@@ -6,13 +6,14 @@ const EvelyngarciaMendozaPage = () => {
     <>
       <MetaTags
         title="EvelyngarciaMendoza"
-        description="EvelyngarciaMendoza page"
+        description="EvelyngarciaMendoza Page"
       />
 
       <h1>Evelyn Garcia-Mendoza's Page</h1>
-      <img src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2017/09/groot-school-meme.jpg" title="Groot" width="100%" height="800" style="border:1px solid black;"/>
-
-    </>
+     
+     <img src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2017/09/groot-school-meme.jpg" alt="Groot's Confused Expression"></img>
+     
+     </>
   )
 }
 
