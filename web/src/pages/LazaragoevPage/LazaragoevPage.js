1b2566a39b1ae851b438659650d0d3f2e7c4103b
LazarAgoev <lazar@DESKTOP-B867FDF.localdomain>
2022-09-14 13:32:13 -0400

    practice

--- web/src/pages/LazaragoevPage/LazaragoevPage.js
+++ web/src/pages/LazaragoevPage/LazaragoevPage.js
@@ -4,7 +4,7 @@ import { MetaTags } from '@redwoodjs/web'
 const LazaragoevPage = () => {
   return (
     <>
-      <MetaTags title="Lazaragoev" description="Lazaragoev page" />
+      <MetaTags title="Lazar Agoev" vdescription="Lazaragoev page" />
 
       <h1>LazarAgoevPage</h1>
       <img src= "https://i.redd.it/vlxhtljarvm91.jpg" alt="Chimaev vs Diaz" width="500" height="600" />
