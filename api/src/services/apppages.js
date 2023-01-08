aa3ddcd13908bf414f14f83a365cce424470f93d
iambillmccann <bill.mccann@gmail.com>
2023-01-08 18:00:24 -0500

    Add service with hardcoded data

---
+++ api/src/services/apppages.js
@@ -0,0 +1,10 @@
+export const getPages = async () => {
+  const pages = [
+    {
+      path: '/billmccann',
+      page: 'BillmccannPage',
+      name: 'billmccann',
+    },
+  ]
+  return pages
+}
