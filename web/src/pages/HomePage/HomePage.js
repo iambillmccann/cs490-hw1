6407e50fa7f2d12c18d914f914d14a98b7395750
iambillmccann <bill.mccann@gmail.com>
2023-01-07 20:41:15 -0500

    Test homepage function for Link

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,16 +1,21 @@
+import { Box, Text } from '@chakra-ui/react'
+
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const HomePage = () => {
+  const getLink = () => {
+    return <Link to={routes.billmccann()}>Bill McCann</Link>
+  }
+
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-      <div>
-        <p>
-          <Link to={routes.billmccann()}>Bill McCann</Link> (use
-          ./web/src/pages/BillmccannPage.js as an example )
-        </p>
-      </div>
+      <Box>
+        <Text>
+          {getLink()} (use ./web/src/pages/BillmccannPage.js as an example )
+        </Text>
+      </Box>
     </>
   )
 }
