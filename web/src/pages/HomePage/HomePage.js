40ab2ebf605f5dc5f61ad0c73f84e757041dcafd
iambillmccann <bill.mccann@gmail.com>
2023-01-09 09:53:57 -0500

    Fix bug

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -3,12 +3,14 @@ import { Box, Text } from '@chakra-ui/react'
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
+const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
       <Box>
         <Text>
-        <Link to={routes.billmccann()}>Bill McCann</Link> (use ./web/src/pages/BillmccannPage.js as an example )
+          <Link to={routes.billmccann()}>Bill McCann</Link> (use
+          ./web/src/pages/BillmccannPage.js as an example )
         </Text>
       </Box>
     </>
