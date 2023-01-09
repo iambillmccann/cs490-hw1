8a918beabc59e452b795679bd11f13785ac8f27a
iambillmccann <bill.mccann@gmail.com>
2023-01-09 09:44:07 -0500

    Reverted the routing function calls

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -3,13 +3,10 @@ import { Router, Route, Set } from '@redwoodjs/router'
 import Hw1Layout from './layouts/Hw1Layout/Hw1Layout'
 
 const Routes = () => {
-  const getRoute = () => {
-    return <Route path="/billmccann" page={BillmccannPage} name="billmccann" />
-  }
   return (
     <Router>
       <Set wrap={Hw1Layout}>
-        {getRoute()}
+        <Route path="/billmccann" page={BillmccannPage} name="billmccann" />
         <Route path="/" page={HomePage} name="home" />
         <Route path="/home" page={HomePage} name="home" />
       </Set>
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -3,17 +3,12 @@ import { Box, Text } from '@chakra-ui/react'
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
-const HomePage = () => {
-  const getLink = () => {
-    return <Link to={routes.billmccann()}>Bill McCann</Link>
-  }
-
   return (
     <>
       <MetaTags title="Home" description="Home page" />
       <Box>
         <Text>
-          {getLink()} (use ./web/src/pages/BillmccannPage.js as an example )
+        <Link to={routes.billmccann()}>Bill McCann</Link> (use ./web/src/pages/BillmccannPage.js as an example )
         </Text>
       </Box>
     </>
