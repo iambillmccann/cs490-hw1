d5f528919742b0bc1fc154a4092ffd45eacf2d79
iambillmccann <bill.mccann@gmail.com>
2023-01-11 16:23:45 -0500

    Add testpage to layout

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -5,9 +5,9 @@ import Hw1Layout from './layouts/Hw1Layout/Hw1Layout'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/test2" page={Test2Page} name="test2" />
       <Set wrap={Hw1Layout}>
         <Route path="/billmccann" page={BillmccannPage} name="billmccann" />
+        <Route path="/test2" page={Test2Page} name="test2" />
         <Route path="/" page={HomePage} name="home" />
         <Route path="/home" page={HomePage} name="home" />
       </Set>
