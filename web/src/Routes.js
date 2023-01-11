0a0c1673ff6dfe3e033af00e63ccf79e933df77d
iambillmccann <bill.mccann@gmail.com>
2023-01-11 16:22:44 -0500

    Add second page to test

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -5,6 +5,7 @@ import Hw1Layout from './layouts/Hw1Layout/Hw1Layout'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/test2" page={Test2Page} name="test2" />
       <Set wrap={Hw1Layout}>
         <Route path="/billmccann" page={BillmccannPage} name="billmccann" />
         <Route path="/" page={HomePage} name="home" />
---
+++ web/src/pages/Test2Page/Test2Page.stories.js
@@ -0,0 +1,7 @@
+import Test2Page from './Test2Page'
+
+export const generated = (args) => {
+  return <Test2Page {...args} />
+}
+
+export default { title: 'Pages/Test2Page' }
