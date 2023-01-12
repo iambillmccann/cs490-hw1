d4ee53a143169dbe9ef0c8d18517a54a95eff48f
iambillmccann <bill.mccann@gmail.com>
2023-01-12 12:57:34 -0500

    Figure out how to determine files are folders

--- scripts/buildhomepage.js
+++ scripts/buildhomepage.js
@@ -2,5 +2,16 @@ console.log('=============================================')
 console.log('Script: buildhomepage.js')
 console.log('---------------------------------------------')
 console.log('Start')
+
+const fileSystem = require('fs')
+const utilities = require('util')
+const folder = '../web/src/pages/'
+
+fileSystem.readdirSync(folder).forEach((file) => {
+  const fullPath = utilities.format('%s%s', folder, file)
+  const stat = fileSystem.statSync(fullPath).isDirectory()
+  console.log(file, fullPath, stat)
+})
+
 console.log('End')
 console.log('=============================================')
