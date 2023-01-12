05f48115fad751b53e52b45894ebd9e014e63422
iambillmccann <bill.mccann@gmail.com>
2023-01-12 14:08:15 -0500

    Grabbed the ending slice

--- scripts/buildhomepage.js
+++ scripts/buildhomepage.js
@@ -8,6 +8,7 @@ const utilities = require('util')
 const folder = '../web/src/pages/'
 const homePage = utilities.format('%s/HomePage/HomePage.js', folder)
 const startTag = '          {/* Start */}'
+const endTag = '          {/* End */}'
 
 // Step 1. Create an array of folders. The folders are files
 //         to include in the Home Page
@@ -22,6 +23,8 @@ const folders = fileSystem.readdirSync(folder).filter((e) => {
   )
 })
 
+console.log(folders)
+
 // Step 2. Read the Home Page into an array
 
 const contents = fileSystem.readFileSync(homePage, 'utf-8')
@@ -32,6 +35,10 @@ const pageContent = contents.split(/\r?\n/)
 const startItem = pageContent.findIndex((item) => item === startTag)
 const startSlice = pageContent.slice(0, startItem + 1)
 console.log(startSlice)
+
+const endItem = pageContent.findIndex((item) => item === endTag)
+const endSlice = pageContent.slice(endItem)
+console.log(endSlice)
 
 console.log('End')
 console.log('=============================================')
