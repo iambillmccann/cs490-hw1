304c85ffe1eddca1b1ec2af1efcab9aeac61228c
mhk37 <112417219+mhk37@users.noreply.github.com>
2023-01-18 18:55:57 -0500

    changing the file
    Added name, github id, discord id and meme

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -5,6 +5,7 @@ import Hw1Layout from './layouts/Hw1Layout/Hw1Layout'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/murtajizkazmi" page={MurtajizkazmiPage} name="murtajizkazmi" />
       <Set wrap={Hw1Layout}>
         <Route path="/billmccann" page={BillmccannPage} name="billmccann" />
         <Route path="/" page={HomePage} name="home" />
---
+++ web/src/pages/MurtajizkazmiPage/MurtajizkazmiPage.stories.js
@@ -0,0 +1,7 @@
+import MurtajizkazmiPage from './MurtajizkazmiPage'
+
+export const generated = (args) => {
+  return <MurtajizkazmiPage {...args} />
+}
+
+export default { title: 'Pages/MurtajizkazmiPage' }
