81bdc3f3808d743e5ef05cd4c9c276a3025bea7e
sureshritika <ritikasuresh19@gmail.com>
2023-01-19 15:08:05 -0500

    update route tag

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -5,8 +5,8 @@ import Hw1Layout from './layouts/Hw1Layout/Hw1Layout'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/ritikasuresh" page={RitikasureshPage} name="ritikasuresh" />
       <Set wrap={Hw1Layout}>
+        <Route path="/ritikasuresh" page={RitikasureshPage} name="ritikasuresh" />
         <Route path="/billmccann" page={BillmccannPage} name="billmccann" />
         <Route path="/" page={HomePage} name="home" />
         <Route path="/home" page={HomePage} name="home" />
