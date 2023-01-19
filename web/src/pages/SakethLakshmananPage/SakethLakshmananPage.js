7a1aab3a432190d62c85ff83def5df8ca7241afb
Saketh Lakshmanan <saketh_lakshmanan@yahoo.com>
2023-01-18 22:41:42 -0500

    Changed sample page to add personal info

---
+++ web/src/pages/SakethLakshmananPage/SakethLakshmananPage.js
@@ -0,0 +1,48 @@
+import {
+  TableContainer,
+  Table,
+  Tr,
+  Td,
+  Tbody,
+  Center,
+  Box,
+} from '@chakra-ui/react'
+
+import { MetaTags } from '@redwoodjs/web'
+
+const SakethLakshmananPage = () => {
+  return (
+    <Box>
+      <MetaTags title="Saketh Lakshmanan" description="Saketh's page" />
+      <screenLeft>
+        <TableContainer width={600} border={'3px'}>
+          <Table variant="simple">
+            <Tbody>
+              <Tr>
+                <Td fontWeight={'Extra Bold'}>NAME:</Td>
+                <Td>Saketh Lakshmanan</Td>
+              </Tr>
+              <Tr>
+                <Td fontWeight={'Extra Bold'}>GITHUB ID:</Td>
+                <Td>sakethl20</Td>
+              </Tr>
+              <Tr>
+                <Td fontWeight={'Extra Bold'}>DISCORD ID:</Td>
+                <Td>sakethl13</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </screenLeft>
+      <br />
+      <screenLeft>
+        <img
+          src="https://gifdb.com/images/high/naruto-your-bestfriend-meme-znd7k18lr3pu80m2.webp"
+          alt="Naruto wants to be your bestfriend"
+        />
+      </screenLeft>
+    </Box>
+  )
+}
+
+export default SakethLakshmananPage
