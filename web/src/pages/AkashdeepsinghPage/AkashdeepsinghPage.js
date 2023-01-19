13ce8f14f9ad6a7084d538b17d08578f4e868eb6
singak1 <a97sin@gmail.com>
2023-01-19 17:52:29 -0500

    Made intial changes to the webpage

--- web/src/pages/AkashdeepsinghPage/AkashdeepsinghPage.js
+++ web/src/pages/AkashdeepsinghPage/AkashdeepsinghPage.js
@@ -1,25 +1,27 @@
-import { TableContainer, Table, Tr, Td, Tbody, Center } from '@chakra-ui/react'
+import { TableContainer, Table, Tr, Td, Tbody, Center, Fade, ScaleFade, Slide, SlideFade, background } from '@chakra-ui/react'
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+
+
 
 const AkashdeepsinghPage = () => {
   return (
     <box>
     <MetaTags title="Akashdeep Singh" description="Akash's page" />
       <Center>
-        <TableContainer width={480} border={'1px'}>
-          <Table variant="simple">
+        <TableContainer width={600} border={'2px'}>
+          <Table variant="striped" colorScheme="gray" >
             <Tbody>
               <Tr>
-                <Td fontWeight={'bolder'}>My Name:</Td>
+                <Td fontWeight={'bolder'} color="Highlight">My Name:</Td>
                 <Td>Akashdeep Singh</Td>
               </Tr>
               <Tr>
-                <Td fontWeight={'bold'}>My GitHub Id:</Td>
+                <Td fontWeight={'bold'} color="Highlight">My GitHub Id:</Td>
                 <Td>singak1</Td>
               </Tr>
               <Tr>
-                <Td fontWeight={'bold'}>My Discord Id:</Td>
+                <Td fontWeight={'bold'} color="Highlight">My Discord Id:</Td>
                 <Td>EasyPeazy21</Td>
               </Tr>
             </Tbody>
@@ -30,7 +32,7 @@ const AkashdeepsinghPage = () => {
       <Center>
         <img
           src="https://media.giphy.com/media/2eTVRPLl4WgOrxY0kr/giphy-downsized-large.gif"
-          alt="No More Errors Please"
+          alt="No More Fooling Me now"
         />
       </Center>
     </box>
