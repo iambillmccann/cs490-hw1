a3b884f0a4b4194a6a445b5cba1b79db50df57b0
Namitha Yalla <75041930+nYalla426@users.noreply.github.com>
2023-01-20 16:20:31 -0500

    Ran yarn rw g scaffold

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -1,10 +1,18 @@
 import { Router, Route, Set } from '@redwoodjs/router'
 
+import PostsLayout from 'src/layouts/PostsLayout'
+
 import Hw1Layout from './layouts/Hw1Layout/Hw1Layout'
 
 const Routes = () => {
   return (
     <Router>
+      <Set wrap={PostsLayout}>
+        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
+        <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
+        <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
+        <Route path="/posts" page={PostPostsPage} name="posts" />
+      </Set>
       <Set wrap={Hw1Layout}>
         <Route path="/billmccann" page={BillmccannPage} name="billmccann" />
         <Route path="/" page={HomePage} name="home" />
