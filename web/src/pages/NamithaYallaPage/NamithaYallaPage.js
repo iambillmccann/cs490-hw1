76ddbf91b1f44a38cfa9cc3c0e934a6a268ac659
Namitha Yalla <75041930+nYalla426@users.noreply.github.com>
2023-01-20 17:04:25 -0500

    edited NamithaYallaPage.js to include my info

--- web/src/pages/NamithaYallaPage/NamithaYallaPage.js
+++ web/src/pages/NamithaYallaPage/NamithaYallaPage.js
@@ -3,19 +3,36 @@ import { MetaTags } from '@redwoodjs/web'
 
 const NamithaYallaPage = () => {
   return (
-    <>
-      <MetaTags title="NamithaYalla" description="NamithaYalla page" />
-
-      <h1>NamithaYallaPage</h1>
-      <p>
-        Find me in{' '}
-        <code>./web/src/pages/NamithaYallaPage/NamithaYallaPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>namithaYalla</code>, link to me with `
-        <Link to={routes.namithaYalla()}>NamithaYalla</Link>`
-      </p>
-    </>
+    <box>
+      <MetaTags title="Namitha Yalla" description="Nemo's page" />
+      <Center>
+        <TableContainer width={480} border={'1px'}>
+          <Table variant="simple">
+            <Tbody>
+              <Tr>
+                <Td fontWeight={'bold'}>My Name:</Td>
+                <Td>(Namitha) Nemo Yalla</Td>
+              </Tr>
+              <Tr>
+                <Td fontWeight={'bold'}>My GitHub Id:</Td>
+                <Td>nYalla6</Td>
+              </Tr>
+              <Tr>
+                <Td fontWeight={'bold'}>My Discord Id:</Td>
+                <Td>nemoooo</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Center>
+      <br />
+      <Center>
+        <img
+          src="https://tenor.com/bHDTo.gif"
+          alt="You just got rick rolled."
+        />
+      </Center>
+    </box>
   )
 }
 
