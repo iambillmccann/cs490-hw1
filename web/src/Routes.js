38212b96f5be2e64378d819a88e08ddddc294a10
jasingh99 <113078673+jasingh99@users.noreply.github.com>
2023-01-20 15:34:39 -0500

    hw1 completed
    added page(gif and info)

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -5,8 +5,8 @@ import Hw1Layout from './layouts/Hw1Layout/Hw1Layout'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/jaspreetsingh" page={JaspreetSinghPage} name="jaspreetsingh" />
       <Set wrap={Hw1Layout}>
+        <Route path="/jaspreetsingh" page={JaspreetSinghPage} name="jaspreetsingh" />
         <Route path="/billmccann" page={BillmccannPage} name="billmccann" />
         <Route path="/" page={HomePage} name="home" />
         <Route path="/home" page={HomePage} name="home" />
