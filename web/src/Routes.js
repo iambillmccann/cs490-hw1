98c6a3556e72a4a99df45ca2a3d8af02bfa88bd7
Namitha Yalla <75041930+nYalla426@users.noreply.github.com>
2023-01-21 14:35:36 -0500

    reverted Routes.js to initial state

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -7,14 +7,7 @@ import Hw1Layout from './layouts/Hw1Layout/Hw1Layout'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/namitha-yalla" page={NamithaYallaPage} name="namithaYalla" />
-      <Set wrap={PostsLayout}>
-        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
-        <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
-        <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
-        <Route path="/posts" page={PostPostsPage} name="posts" />
-      </Set>
-      <Set wrap={Hw1Layout}>
+        <Set wrap={Hw1Layout}>
         <Route path="/billmccann" page={BillmccannPage} name="billmccann" />
         <Route path="/" page={HomePage} name="home" />
         <Route path="/home" page={HomePage} name="home" />
