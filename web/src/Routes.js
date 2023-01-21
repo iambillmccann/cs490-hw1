efc397c29703985c8d5ae6ed95a495c4936f0369
Sai Vedagiri <43285226+SaiVedagiri@users.noreply.github.com>
2023-01-20 20:40:24 -0500

    Add page data and styling

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -5,8 +5,8 @@ import Hw1Layout from './layouts/Hw1Layout/Hw1Layout'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/saivedagiri" page={SaivedagiriPage} name="saivedagiri" />
       <Set wrap={Hw1Layout}>
+        <Route path="/saivedagiri" page={SaivedagiriPage} name="saivedagiri" />
         <Route path="/billmccann" page={BillmccannPage} name="billmccann" />
         <Route path="/" page={HomePage} name="home" />
         <Route path="/home" page={HomePage} name="home" />
