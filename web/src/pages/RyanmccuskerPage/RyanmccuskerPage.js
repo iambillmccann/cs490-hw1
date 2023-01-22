94cda98286045d210acc0733c983377242412b59
rjm8 <rjm8@njit.edu>
2023-01-22 16:38:22 -0500

    forgot that I have a last name

--- web/src/pages/RyanmccuskerPage/RyanmccuskerPage.js
+++ web/src/pages/RyanmccuskerPage/RyanmccuskerPage.js
@@ -7,7 +7,7 @@ const RyanmccuskerPage = () => {
       <MetaTags title="Ryan McCusker" description="Ryan McCusker's page" />
 
       <div align="center">
-        <p><b>Name:</b> Ryan</p>
+        <p><b>Name:</b> Ryan McCusker</p>
         <p><b>GitHub:</b> rjm8</p>
         <p><b>Discord:</b> potentialpaper#4579</p>
         <img src="https://images.pexels.com/photos/631546/pexels-photo-631546.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="funny guy" />
