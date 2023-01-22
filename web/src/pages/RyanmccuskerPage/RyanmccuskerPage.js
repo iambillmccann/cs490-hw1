987de227a28fe23602153c238ce97dde7cc0a622
rjm8 <rjm8@njit.edu>
2023-01-22 16:29:19 -0500

    update /ryanmccusker

--- web/src/pages/RyanmccuskerPage/RyanmccuskerPage.js
+++ web/src/pages/RyanmccuskerPage/RyanmccuskerPage.js
@@ -4,17 +4,15 @@ import { MetaTags } from '@redwoodjs/web'
 const RyanmccuskerPage = () => {
   return (
     <>
-      <MetaTags title="Ryanmccusker" description="Ryanmccusker page" />
+      <MetaTags title="Ryan McCusker" description="Ryan McCusker's page" />
 
-      <h1>RyanmccuskerPage</h1>
-      <p>
-        Find me in{' '}
-        <code>./web/src/pages/RyanmccuskerPage/RyanmccuskerPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>ryanmccusker</code>, link to me with `
-        <Link to={routes.ryanmccusker()}>Ryanmccusker</Link>`
-      </p>
+      <div align="center">
+        <p><b>Name:</b> Ryan</p>
+        <p><b>GitHub:</b> rjm8</p>
+        <p><b>Discord:</b> potentialpaper#4579</p>
+        <img src="https://images.pexels.com/photos/631546/pexels-photo-631546.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="funny guy" />
+        <p>I think this is a funny meme because his face is weird</p>
+      </div>
     </>
   )
 }
