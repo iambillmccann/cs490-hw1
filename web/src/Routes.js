835c32a7e7926ae369919f896c3e39159982f625
jill3 <jill30jack@gmail.com>
2023-01-22 17:02:25 -0500

    changed gif

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -5,7 +5,7 @@ import Hw1Layout from './layouts/Hw1Layout/Hw1Layout'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/jillianjacinto" page={JillianjacintoPage} name="jillianjacinto" />
+
       <Set wrap={Hw1Layout}>
         <Route path="/deanna-mostafa" page={DeannaMostafaPage} name="deannaMostafa" />
         <Route path="/ritikasuresh" page={RitikasureshPage} name="ritikasuresh" />
@@ -17,6 +17,7 @@ const Routes = () => {
         <Route path="/crismolina" page={CrismolinaPage} name="crismolina" />
         <Route path="/billmccann" page={BillmccannPage} name="billmccann" />
         <Route path="/arijqureshi" page={ArijqureshiPage} name="arijqureshi" />
+        <Route path="/jillianjacinto" page={JillianjacintoPage} name="jillianjacinto" />
         <Route path="/" page={HomePage} name="home" />
         <Route path="/home" page={HomePage} name="home" />
       </Set>
