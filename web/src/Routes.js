5685f1a76bd4f507f21de7ca6d711330ecc0d6d8
ga288 <ga288@njit.edu>
2023-01-22 14:02:15 -0500

    1/22 HW1
    Changed the file ./web/src/pages/{yourname}/{yourname}.js
       Add your name, GitHub Id, Discord name, and a favorite meme

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -5,6 +5,7 @@ import Hw1Layout from './layouts/Hw1Layout/Hw1Layout'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/ga288" page={Ga288Page} name="ga288" />
       <Set wrap={Hw1Layout}>
         <Route path="/deanna-mostafa" page={DeannaMostafaPage} name="deannaMostafa" />
         <Route path="/ritikasuresh" page={RitikasureshPage} name="ritikasuresh" />
