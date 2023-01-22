aa2b0a7d2f48650d6b523bb4efd9f6a490d9ea96
SeanUlep <su25@njit.edu>
2023-01-21 22:35:16 -0500

    Finished
    Modifications to the website

--- web/src/pages/SeanUlepPage/SeanUlepPage.js
+++ web/src/pages/SeanUlepPage/SeanUlepPage.js
@@ -3,18 +3,36 @@ import { MetaTags } from '@redwoodjs/web'
 
 const SeanUlepPage = () => {
   return (
-    <>
-      <MetaTags title="SeanUlep" description="SeanUlep page" />
-
-      <h1>SeanUlepPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/SeanUlepPage/SeanUlepPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>seanUlep</code>, link to me with `
-        <Link to={routes.seanUlep()}>SeanUlep</Link>`
-      </p>
-    </>
+    <box>
+      <MetaTags title="Sean Ulep" description="The Professor Pupil's Page" />
+      <Center>
+        <TableContainer width={480} border={'5px'}>
+          <Table variant="simple">
+            <Tbody>
+              <Tr>
+                <Td fontWeight={'heavy'}>My Name:</Td>
+                <Td>Sean Ulep</Td>
+              </Tr>
+              <Tr>
+                <Td fontWeight={'heavy'}>My GitHub Id:</Td>
+                <Td>SeanUlep</Td>
+              </Tr>
+              <Tr>
+                <Td fontWeight={'heavy'}>My Discord Id:</Td>
+                <Td>Shaun_Tulip or su25 (server username)</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Center>
+      <br />
+      <Center>
+        <img
+          src="https://media.giphy.com/media/u5GxLmudnA8vK/giphy.gif"
+          alt="Kirby nomming on a watermelon"
+        />
+      </Center>
+    </box>
   )
 }
 
