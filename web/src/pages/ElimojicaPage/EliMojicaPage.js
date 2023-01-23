d87fad750aad7082d7d13ddb488343f04ed24837
Eli Mojica <el.mojica01@gmail.com>
2023-01-22 20:27:57 -0500

    fixed the set tag and changed the formatting

--- web/src/pages/ElimojicaPage/EliMojicaPage.js
+++ web/src/pages/ElimojicaPage/EliMojicaPage.js
@@ -3,26 +3,36 @@ import { MetaTags } from '@redwoodjs/web'
 
 const ElimojicaPage = () => {
   return (
-    <>
-      <MetaTags title="Elimojica" description="Elimojica page" />
-
-      <h1>Eli Mojica</h1>
-      <p>
-        Find me in <code>./web/src/pages/ElimojicaPage/ElimojicaPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>elimojica</code>, link to me with `
-        <Link to={routes.elimojica()}>Elimojica</Link>`
-      </p>
-
-      <p>Discord Account: Eli#0714</p>
-      <p>
-        Github Account: <a href="https://github.com/Elmojica">Elmojica</a>
-      </p>
-
-      <img src="https://media.tenor.com/do8q_eYrsW4AAAAd/crying-black-guy-meme.gif"></img>
-    
-    </>
+      <box>
+      <MetaTags title="Eli Mojica" description="Eli's page" />
+      <Center>
+        <TableContainer width={480} border={'1px'}>
+          <Table variant="simple">
+            <Tbody>
+              <Tr>
+                <Td fontWeight={'bold'}>My Name:</Td>
+                <Td>Eli Mojica</Td>
+              </Tr>
+              <Tr>
+                <Td fontWeight={'bold'}>My GitHub Id:</Td>
+                <Td>elmojica</Td>
+              </Tr>
+              <Tr>
+                <Td fontWeight={'bold'}>My Discord Id:</Td>
+                <Td>eli#0714</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Center>
+      <br />
+      <Center>
+        <img
+          src="https://media.tenor.com/do8q_eYrsW4AAAAd/crying-black-guy-meme.gif"
+          alt="Guy crying"
+        />
+      </Center>
+    </box>
   )
 }
 
