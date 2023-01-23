4a8f8182ee9e9452fa7f903ae83e12c92d9fc246
kpatel2002 <knp59@njit.edu>
2023-01-23 00:03:26 -0500

    update4

--- web/src/pages/KaranPatelPage/KaranPatelPage.js
+++ web/src/pages/KaranPatelPage/KaranPatelPage.js
@@ -11,15 +11,15 @@ const KaranpatelPage = () => {
           <Table variant="simple">
             <Tbody>
               <Tr>
-                <Td fontWeight={'bold'}>My Name:</Td>
+                <Td fontWeight={'italic'}>My Name:</Td>
                 <Td>Karan Patel</Td>
               </Tr>
               <Tr>
-                <Td fontWeight={'bold'}>My GitHub Id:</Td>
+                <Td fontWeight={'italic'}>My GitHub Id:</Td>
                 <Td>karanpatel2002</Td>
               </Tr>
               <Tr>
-                <Td fontWeight={'bold'}>My Discord Id:</Td>
+                <Td fontWeight={'italic'}>My Discord Id:</Td>
                 <Td>KaranPatel</Td>
               </Tr>
             </Tbody>
