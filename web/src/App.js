6417b188171a39d446200fdffa2be39134b95843
manthan2905 <mp876@njit.edu>
2023-01-23 17:19:12 -0500

    app.js fix

--- web/src/App.js
+++ web/src/App.js
@@ -6,7 +6,6 @@ import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
-import './scaffold.css'
 import './index.css'
 
 const App = () => (
