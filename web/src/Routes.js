cb7d39bc6d66d2a9e0ed569bf224b525eeca43a6
iambillmccann <bill.mccann@gmail.com>
2023-01-23 13:46:05 -0500

    Make changes to get working

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -5,17 +5,17 @@ import Hw1Layout from './layouts/Hw1Layout/Hw1Layout'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/gabrielpascual" page={GabrielpascualPage} name="gabrielpascual" />
       <Set wrap={Hw1Layout}>
+        <Route path="/gabrielpascual" page={GabrielpascualPage} name="gabrielpascual" />
         <Route path="/mehakmaqsood" page={MehakmaqsoodPage} name="mehakmaqsood" />
         <Route path="/mateamilloshi" page={MateamilloshiPage} name="mateamilloshi" />
         <Route path="/pedropacheco" page={PedropachecoPage} name="pedropacheco" />
-        <Route path="/erik-chodaba" page={ErikChodabaPage} name="erikChodaba" />
+        <Route path="/erik-chodaba" page={ErikChodabaPage} name="erikchodaba" />
         <Route path="/nehashirwalker" page={NehashirwalkerPage} name="nehashirwalker" />
-        <Route path="/deanna-mostafa" page={DeannaMostafaPage} name="deannaMostafa" />
+        <Route path="/deanna-mostafa" page={DeannaMostafaPage} name="deannamostafa" />
         <Route path="/ritikasuresh" page={RitikasureshPage} name="ritikasuresh" />
-        <Route path="/saketh-lakshmanan" page={SakethLakshmananPage} name="sakethLakshmanan" />
-        <Route path="/safiullah-baig" page={SafiullahBaigPage} name="safiullahBaig" />
+        <Route path="/saketh-lakshmanan" page={SakethLakshmananPage} name="sakethlakshmanan" />
+        <Route path="/safiullah-baig" page={SafiullahBaigPage} name="safiullahbaig" />
         <Route path="/saivedagiri" page={SaivedagiriPage} name="saivedagiri" />
         <Route path="/jaspreetsingh" page={JaspreetSinghPage} name="jaspreetsingh" />
         <Route path="/keyadhruve" page={KeyadhruvePage} name="keyadhruve" />
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -10,23 +10,22 @@ const HomePage = () => {
       <Box>
         <Text>
           {/* Start */}
-          <Link to={routes.arijqureshi()}>arijqureshi</Link>
-          <Link to={routes.billmccann()}>billmccann</Link>
-          <Link to={routes.crismolina()}>crismolina</Link>
-          <Link to={routes.deannamostafa()}>deannamostafa</Link>
-          <Link to={routes.erikchodaba()}>erikchodaba</Link>
-          <Link to={routes.gabrielpascual()}>gabrielpascual</Link>
-          <Link to={routes.jaspreetsingh()}>jaspreetsingh</Link>
-          <Link to={routes.johndoe()}>johndoe</Link>
-          <Link to={routes.keyadhruve()}>keyadhruve</Link>
-          <Link to={routes.mateamilloshi()}>mateamilloshi</Link>
-          <Link to={routes.mehakmaqsood()}>mehakmaqsood</Link>
-          <Link to={routes.nehashirwalker()}>nehashirwalker</Link>
-          <Link to={routes.pedropacheco()}>pedropacheco</Link>
-          <Link to={routes.ritikasuresh()}>ritikasuresh</Link>
-          <Link to={routes.safiullahbaig()}>safiullahbaig</Link>
-          <Link to={routes.saivedagiri()}>saivedagiri</Link>
-          <Link to={routes.sakethlakshmanan()}>sakethlakshmanan</Link>
+          <Link to={routes.arijqureshi()}>arijqureshi</Link><br />
+          <Link to={routes.billmccann()}>billmccann</Link><br />
+          <Link to={routes.crismolina()}>crismolina</Link><br />
+          <Link to={routes.deannamostafa()}>deannamostafa</Link><br />
+          <Link to={routes.erikchodaba()}>erikchodaba</Link><br />
+          <Link to={routes.gabrielpascual()}>gabrielpascual</Link><br />
+          <Link to={routes.jaspreetsingh()}>jaspreetsingh</Link><br />
+          <Link to={routes.keyadhruve()}>keyadhruve</Link><br />
+          <Link to={routes.mateamilloshi()}>mateamilloshi</Link><br />
+          <Link to={routes.mehakmaqsood()}>mehakmaqsood</Link><br />
+          <Link to={routes.nehashirwalker()}>nehashirwalker</Link><br />
+          <Link to={routes.pedropacheco()}>pedropacheco</Link><br />
+          <Link to={routes.ritikasuresh()}>ritikasuresh</Link><br />
+          <Link to={routes.safiullahbaig()}>safiullahbaig</Link><br />
+          <Link to={routes.saivedagiri()}>saivedagiri</Link><br />
+          <Link to={routes.sakethlakshmanan()}>sakethlakshmanan</Link><br />
           {/* End */}
         </Text>
       </Box>
@@ -35,4 +34,5 @@ const HomePage = () => {
 }
 
 export default HomePage
+
 
