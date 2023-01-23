31a1956d218792517a40042a0965cf4a008a72d7
kpatel2002 <knp59@njit.edu>
2023-01-23 00:02:23 -0500

    Update KaranPatelPage.js

--- web/src/pages/KaranPatelPage/KaranPatelPage.js
+++ web/src/pages/KaranPatelPage/KaranPatelPage.js
@@ -2,7 +2,7 @@ import { TableContainer, Table, Tr, Td, Tbody, Center } from '@chakra-ui/react'
 
 import { MetaTags } from '@redwoodjs/web'
 
-const KaranPage = () => {
+const KaranpatelPage = () => {
   return (
     <box>
       <MetaTags title="Karan Patel" description="Karan's page" />
