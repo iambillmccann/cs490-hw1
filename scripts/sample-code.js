577ab41c61eae095c4f3d51140f13e6e872d84da
Bill McCann <bill.mccann@gmail.com>
2023-01-24 19:10:22 -0500

    Samples

---
+++ scripts/sample-code.js
@@ -0,0 +1,4 @@
+const x = 2
+const y = '2'
+console.log(x == y) // Returns true since the value of both x and y is the same
+console.log(x === y) // Returns false since the typeof x is "number" and typeof y is "string
